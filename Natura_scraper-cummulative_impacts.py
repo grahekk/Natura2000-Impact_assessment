da1097f7a1d66c66cea7876e09433522ae54b970
grahekk <98557014+grahekk@users.noreply.github.com>
2023-03-13 11:03:25 +0100

    Scraper updated
    Scraper is functional and puts all the files in the cummulative_download folder

--- Natura_scraper-cummulative_impacts.py
+++ Natura_scraper-cummulative_impacts.py
@@ -1,22 +1,42 @@
+import os
+
 import requests
 from bs4 import BeautifulSoup
-import urllib.request
 
-# URL of the page to scrape
+
+# The URL of the page to scrape
 url = "https://mingor.gov.hr/o-ministarstvu-1065/djelokrug/uprava-za-procjenu-utjecaja-na-okolis-i-odrzivo-gospodarenje-otpadom-1271/procjena-utjecaja-na-okolis-puo-spuo/procjena-utjecaja-zahvata-na-okolis-puo-4014/4014"
+#folder where the data will be saved
+dir = "cummulative_download"
+if not os.path.exists(dir):
+    os.mkdir(dir)
 
-# Make a request to the URL
+# Send a request to the URL and get the HTML response
 response = requests.get(url)
 
-# Parse the HTML content using BeautifulSoup
+# Parse the HTML response using BeautifulSoup
 soup = BeautifulSoup(response.content, "html.parser")
 
-# Find all list items in the HTML that contain the text "PUO rješenje"
-li_items = soup.find_all("li", string=lambda text: "PUO rješenje" in str(text))
+# Find all <div> elements with class "PUO postupci"
+postupci_divs = soup.find_all("div", class_="faqOdgovor")
+#print(postupci_divs)
+# Loop through each <div> element and find the <li> elements with "PUO rješenje" text
 
-# Loop through each list item and download the corresponding PDF file
-for li_item in li_items:
-    # Find the nested link element and get the href attribute
-    href = li_item.find("a").get("href")
-    # Download the PDF file
-    urllib.request.urlretrieve(href, li_item.text.strip() + ".pdf")
+# Loop through each <div> element and find the <a> elements with "data-fileid" attribute
+for postupak_div in postupci_divs:
+    print(postupak_div)
+    pdf_links = postupak_div.find_all("a",
+                                      {"data-fileid": True, "href": True, "target": "_blank", "title": "PUO rješenje"})
+    #pdf_links = postupak_div.find_all("a")
+    print(pdf_links)
+    # Loop through each <a> element and download the PDF file
+    for pdf_link in pdf_links:
+        pdf_url = pdf_link["href"]
+        pdf_name = pdf_url.split("/")[-1]  # Get the file name from the URL
+
+        # Send a request to the PDF URL and download the file
+        pdf_response = requests.get(pdf_url)
+        with open(pdf_name, "wb") as f:
+            f.write(pdf_response.content)
+
+        print(f"{pdf_name} downloaded.")
