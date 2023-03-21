73f4ff1b2404f6750342c96559566aa6972338fb
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-03-21 15:44:06 +0100

    Update Natura_map.py

--- Natura_map.py
+++ Natura_map.py
@@ -1,5 +1,18 @@
 import geopandas as gpd
 
+# Define the WFS URL and the layer name
+wfs_url = 'https://services.bioportal.hr/wfs'
+layer_name = 'example_layer'
+
+# Use geopandas to get the available layers on the WFS
+wfs_layers = gpd.read_file(wfs_url, driver='WFS')
+
+# Print the layer names
+print(wfs_layers['Name'])
+
+# Use geopandas to load the data from the WFS
+polygons = gpd.read_file(wfs_url, layer=layer_name, crs='EPSG:3765')
+
 # Load the GeoDataFrames
 points = gpd.read_file("path/to/points.shp")
 polygons = gpd.read_file("path/to/polygons.shp")
