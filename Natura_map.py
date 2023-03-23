28b2089fd52fe0e494416d109d59b5681e9e723b
Nikola Geršak <98557014+grahekk@users.noreply.github.com>
2023-03-23 16:13:24 +0100

    New script for initializing qgis added, natura_map repaired

--- Natura_map.py
+++ Natura_map.py
@@ -1,37 +1,47 @@
-from owslib.wms import WebMapService
-from owslib.wfs import WebFeatureService
+import os
+
 import geopandas as gpd
+from owslib.wfs import WebFeatureService
 
-# Define the WFS URL and the layer name
-wfs_url = 'https://services.bioportal.hr/wms'
+dir = "geospatial_downloads"
+if not os.path.exists(dir):
+    os.mkdir(dir)
 
-# Use owslib to get the data from the WFS
-wfs11 = WebFeatureService(url=wfs_url, version='1.1.0')
-wfs_contents = list(wfs11.contents)
-layers_to_select = ['dzzpnpis:direktiva_o_stanistima_natura2000_hr_2019_',
-                    'dzzpnpis:direktiva_o_pticama_natura2000_hr_2019_']
 
-# select them layers
-POVS_layer = wfs11.getfeature(typename=layers_to_select[0])
-POP_layer = wfs11.getfeature(typename=layers_to_select[1])
+def get_natura_layer():
+    # url from which the layers are downloaded
+    wfs_url = 'https://services.bioportal.hr/wms'
+    # natura layers to be downluaded from wfs
+    layers_to_select = ['dzzpnpis:direktiva_o_stanistima_natura2000_hr_2019_',
+                        'dzzpnpis:direktiva_o_pticama_natura2000_hr_2019_']
+    # first check if the file already exists, and then make the request
+    natura_layers = []
+    for i, j in enumerate(layers_to_select):
+        layer_to_select = layers_to_select[i]
+        file_name = layer_to_select.split(":")[1]
+        file = os.path.join(dir, file_name + ".kml")
+        if not os.path.exists(f"{dir}/{file_name}.kml"):
+            # Use owslib to get the data from the WFS
+            wfs11 = WebFeatureService(url=wfs_url, version='1.1.0')
+            # select them layers and get the response
+            response = wfs11.getfeature(typename=layer_to_select)
+            with open(file, 'wb') as f:
+                f.write(response.read())
+            print(f"{file_name} downloaded.")
+        else:
+            print(f"{file_name} already exists, skipping download")
+        natura_layers.append(file)
+    return natura_layers
 
-# Use geopandas to convert the data to a GeoDataFrame
-features = POVS_layer.json()['features']
-geojson = {'type': 'FeatureCollection', 'features': features}
-polygons = gpd.read_file(geojson=geojson)
+
+layers = get_natura_layer()
+# read the polygons
+polygons = gpd.read_file(layers[0], crs='EPSG:3765')
 polygons.plot()
-# Use geopandas to get the available layers on the WFS
-wfs_layers = gpd.read_file(wfs_url, driver='WFS')
 
-# Print the layer names
-print(wfs_layers['Name'])
-
-# Use geopandas to load the data from the WFS
-polygons = gpd.read_file(wfs_url, layer=layer_name, crs='EPSG:3765')
-
-# Load the GeoDataFrames
+# extract by the location part
+# Load the main shp
 points = gpd.read_file("path/to/points.shp")
-polygons = gpd.read_file("path/to/polygons.shp")
 
 # Create a buffer around the points
 buffered_points = points.buffer(distance=100)  # 100 is the buffer distance in the unit of the points' CRS
